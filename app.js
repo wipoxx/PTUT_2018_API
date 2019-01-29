a8d503995de6cd6c520188c7b742876f045480cf
mehdilabourdette <mehdi.labourdette@gmail.com>
2019-01-29 08:56:12 +0100

    Ajout du header CORS pour permettre la requête depuis le front

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add CORS header
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Listen on port
 app.listen(1234);
 
